11475773e47df66a6f8f4f48ec37f2185e403b26
Pascal Breuninger <pascal.breuninger@loft.sh>
2025-08-29 07:53:30 +0200

    initial commit

---
+++ environment/main.tf
@@ -0,0 +1,173 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+locals {
+  resource_group_name = var.vcluster.requirements["resource-group"]
+  location            = var.vcluster.requirements["location"]
+
+  vcluster_name      = nonsensitive(var.vcluster.instance.metadata.name)
+  vcluster_namespace = var.vcluster.instance.metadata.namespace
+
+  vnet_name           = "${var.vcluster.name}-${random_id.vnet_suffix.hex}-vnet"
+  vnet_cidr           = "10.0.0.0/16"
+  public_subnet_cidr  = "10.0.2.0/24"
+  public_subnet_name  = "${local.vcluster_name}-public"
+  private_subnet_cidr = "10.0.1.0/24"
+  private_subnet_name = "${local.vcluster_name}-private"
+}
+
+resource "random_id" "vnet_suffix" {
+  byte_length = 4
+}
+
+provider "azurerm" {
+  features {}
+  use_cli = false
+}
+
+# Networking
+resource "azurerm_virtual_network" "vnet" {
+  name                = local.vnet_name
+  address_space       = [local.vnet_cidr]
+  location            = local.location
+  resource_group_name = local.resource_group_name
+}
+
+resource "azurerm_subnet" "private" {
+  name                 = local.private_subnet_name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = [local.private_subnet_cidr]
+}
+
+resource "azurerm_subnet" "public" {
+  name                 = local.public_subnet_name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = [local.public_subnet_cidr]
+}
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${local.vcluster_name}-public-ip"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                = "${local.vcluster_name}-nat-gateway"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  sku_name            = "Standard"
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.public_ip.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "private" {
+  nat_gateway_id = azurerm_nat_gateway.main.id
+  subnet_id      = azurerm_subnet.private.id
+}
+
+# Security Groups
+resource "azurerm_network_security_group" "public" {
+  name                = "${local.vcluster_name}-nsg-public"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  # Allow HTTP inbound
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow HTTPS inbound
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  # Allow SSH inbound
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_security_group" "private" {
+  name                = "${local.vcluster_name}-nsg-private"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  # Allow inbound traffic from public subnet
+  security_rule {
+    name                       = "AllowFromPublicSubnet"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = local.public_subnet_cidr
+    destination_address_prefix = "*"
+  }
+
+  # Deny all other inbound traffic from internet
+  security_rule {
+    name                       = "DenyInternetInbound"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "public" {
+  subnet_id                 = azurerm_subnet.public.id
+  network_security_group_id = azurerm_network_security_group.public.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private.id
+  network_security_group_id = azurerm_network_security_group.private.id
+}
---
+++ node/main.tf
@@ -0,0 +1,92 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_cli = false
+}
+
+locals {
+  resource_group_name = var.vcluster.requirements["resource-group"]
+  location            = var.vcluster.requirements["location"]
+
+  vcluster_name      = var.vcluster.instance.metadata.name
+  vcluster_namespace = var.vcluster.instance.metadata.namespace
+
+  vm_name           = "${var.vcluster.name}-${random_id.vm_suffix.hex}"
+  private_subnet_id = var.vcluster.nodeEnvironment.outputs["private_subnet_id"]
+  instance_type     = var.vcluster.nodeType.spec.properties["instance-type"]
+}
+
+resource "random_id" "vm_suffix" {
+  byte_length = 4
+}
+
+resource "azurerm_network_interface" "private_vm" {
+  name                = "${local.vm_name}-nic"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = local.private_subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# The node is joined via cloud-init user data
+# We don't need the private key
+resource "tls_private_key" "vm_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "azurerm_linux_virtual_machine" "private_vm" {
+  name                = local.vm_name
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = local.instance_type
+  admin_username      = "azureuser"
+
+  disable_password_authentication = true
+
+  network_interface_ids = [
+    azurerm_network_interface.private_vm.id,
+  ]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.vm_ssh.public_key_openssh
+  }
+
+  user_data = base64encode(var.vcluster.userData)
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 100
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+
+  tags = {
+    vcluster  = local.vcluster_name
+    namespace = local.vcluster_namespace
+  }
+}
