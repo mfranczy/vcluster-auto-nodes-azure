d7b7addf4a161023dd3362f89bc37f42b2c899a0
Marcin Franczyk <marcin0franczyk@gmail.com>
2025-10-10 20:27:39 +0200

    test

--- environment/infrastructure/nsg.tf
+++ environment/infrastructure/nsg.tf
@@ -4,56 +4,20 @@ resource "azurerm_network_security_group" "workers" {
   resource_group_name = local.resource_group_name
 
   security_rule {
-    name                       = "allow-intra-vnet"
-    priority                   = 1000
+    name                       = "allow-all-Inbound"
+    priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "*"
     source_port_range          = "*"
     destination_port_range     = "*"
-    source_address_prefix      = local.vnet_cidr_block
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow-kubelet"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "10250"
-    source_address_prefix      = local.vnet_cidr_block
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow-nodeport-range"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "30000-32767"
-    source_address_prefix      = local.vnet_cidr_block
-    destination_address_prefix = "*"
-  }
-
-  security_rule {
-    name                       = "allow-ssh-from-vnet"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = local.vnet_cidr_block
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
   security_rule {
     name                       = "allow-all-outbound"
-    priority                   = 1000
+    priority                   = 100
     direction                  = "Outbound"
     access                     = "Allow"
     protocol                   = "*"
